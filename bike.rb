423f1f66087ce6defcea850918bf155b3fdd2853
Jackson <jackson.cutler@gmail.com>
2020-02-11 12:10:53 -0800

    Class 10 Exercise

--- bike.rb
+++ bike.rb
@@ -3,21 +3,26 @@
 class Bike
 
   STANDARD_WEIGHT = 200 # lbs
-  MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
 
-  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+  attr_accessor :id, :color, :price, :weight
+
+  def initialize(id, color, price, weight = STANDARD_WEIGHT)
     @id = id
     @color = color
     @price = price
     @weight = weight
-    @rented = rented
-    @cargo_contents = []
   end
+end
 
-  def rent!
-    self.rented = true
+class Cargo
+
+  MAX_CARGO_ITEMS = 10
+
+  attr_accessor :cargo_contents
+
+  def initialize()
+    @cargo_contents = []
   end
 
   def add_cargo(item)
@@ -35,5 +40,18 @@ class Bike
   def pannier_remaining_capacity
     MAX_CARGO_ITEMS - self.cargo_contents.size
   end
-
 end
+
+class Rental
+
+  def initialize(bike)
+    @bike = bike
+  end
+end
+
+
+
+
+
+
+
